49a433952a2b067ba3aa391be98199ef270509c4
Minamium <arcadia.battery@gmail.com>
2025-12-18 20:24:37 +0900

    修正

--- tests/BNN.py
+++ tests/BNN.py
@@ -1,3 +1,6 @@
+
+
+
 from PyBCA.cli_simClass import BCA_Simulator
 from PyBCA import lib, load_state_conversions_from_yaml
 
@@ -11,13 +14,13 @@ simulator = BCA_Simulator(cellspace_path, rule_paths, device="cuda",
                               spatial_event_filePath="PyBCA/Sample/Specialevent/BNN_event.py",
                               use_tqdm=True,
                               trial_constant_sweep = {
-                                  "join_err_0_input": {"base": 0.0,    "delta": 0.00005},
-                                  "join_err_1_input": {"base": 0.0,    "delta": 0.00001},
-                                  "fork_err_0_input": {"base": 0.0,    "delta": 0.00005},
+                                  "join_err_0_input": {"base": 0.0,    "delta": 0.000005},
+                                  "join_err_1_input": {"base": 0.0,    "delta": 0.000001},
+                                  "fork_err_0_input": {"base": 0.0,    "delta": 0.000005},
                                  })
 
 simulator.Allocate_torch_Tensors_on_Device()
-simulator.set_ParallelTrial(10)
+simulator.set_ParallelTrial(2000)
 
 steps = 80_000
 
@@ -27,7 +30,7 @@ simulator.run_steps(steps=steps,
                     debug=False, 
                     debug_per_trial=False, 
                     state_gate_enable=True, 
-                    state_gate_interval=300)
+                    state_gate_interval=500)
 
 simulator.save_event_histry_for_dataframe("event_history.jsonl", 
                                            format="jsonl_trials", 
